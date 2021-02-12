31c65e74f4c57ae6687a0d5f135f2f49c8e67241
Safayet-masum <47150686+Safayet-masum@users.noreply.github.com>
2021-02-12 17:58:58 +0600

    Update say.m

--- say.m
+++ say.m
@@ -1,5 +1,6 @@
 function d = say(y)
   files = dir('*.wav');
+  done=0;
   for i=1:length(files)
     disp(files(i).name);
     [ref,fs]=audioread(files(i).name);
@@ -7,8 +8,12 @@ function d = say(y)
     x=[info.Title];
     if size(x)==size(y) && x==y
       sound(ref);
+      done=1;
       break;
      end
+  end
+  if done==0
+    warndlg(strcat(y,' not found in the dictionary'));
   end
   
 endfunction
