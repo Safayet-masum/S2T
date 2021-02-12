4039f2f546123beef70220d40e98546cde18f3e1
Safayet-masum <47150686+Safayet-masum@users.noreply.github.com>
2021-02-12 16:43:53 +0600

    Update main.m

--- main.m
+++ main.m
@@ -3,12 +3,15 @@ ch = char(10);
 userPrompt = strcat('Put 0 to Quit',ch,'Put 1 to add new word to dictionary', ch ,'Put 2 for Text to speech', ch ,'Put 3 for Speech to text');
 word = inputdlg(userPrompt, titleBar, 1);
 d=cell2mat(word);
-if d==1
+if d=='1'
   NewWord;
-elseif d==2
+elseif d=='2'
+  d=1
   TextToSpeech;
-elseif d==3
+elseif d=='3'
   SpeechToText;
+else
+  msgbox('Thanks!');
 end;
 
 
